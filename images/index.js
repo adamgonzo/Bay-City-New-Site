02001860542e0f85f64a4f81ef7fb28c9e58c492
Adam Gonzalez <adam.gonz@outlook.com>
2023-10-04 21:16:57 -0700

    Added photo
    Logo Photo

--- images/index.js
+++ images/index.js
@@ -1,33 +1,33 @@
-import picture1 from "./123_1.jpeg";
-import picture2 from "./123_1 2.jpeg";
-import picture3 from "./123_1 3.jpeg";
-import picture4 from "./123_1 4.jpeg";
-import picture5 from "./123_1 5.jpeg";
-import picture6 from "./123_1 6.jpeg";
-import picture7 from "./123_1 7.jpeg";
-import picture8 from "./123_1 8.jpeg";
-import picture9 from "./123_1 9.jpeg";
-import picture10 from "./123_1 10.jpeg";
-import picture11 from "./123_1 11.jpeg";
-import picture12 from "./123_1 12.jpeg";
-import picture13 from "./123_1 13.jpeg";
-import picture14 from "./123_1 14.jpeg";
-import picture15 from "./123_1 15.jpeg";
+import stairRailing1 from "./123_1 3.jpeg";
+import stairRailing2 from "./123_1 9.jpeg";
+import stairRailing3 from "./123_1.jpeg";
+import stairRailing4 from "./123_1 12.jpeg";
+import shower1 from "./123_1 5.jpeg";
+import shower2 from "./123_1 6.jpeg";
+import shower3 from "./123_1 7.jpeg";
+import shower4 from "./123_1 8.jpeg";
+import shower5 from "./123_1 10.jpeg";
+import shower6 from "./123_1 14.jpeg";
+import shower7 from "./123_1 11.jpeg";
+import storefront1 from "./123_1 13.jpeg";
+import storefront2 from "./123_1 4.jpeg";
+import storefront3 from "./123_1 15.jpeg";
+import storefront4 from "./123_1 2.jpeg";
 
 export {
-  picture1,
-  picture2,
-  picture3,
-  picture4,
-  picture5,
-  picture6,
-  picture7,
-  picture8,
-  picture9,
-  picture10,
-  picture11,
-  picture12,
-  picture13,
-  picture14,
-  picture15,
+  shower1,
+  shower2,
+  shower3,
+  shower4,
+  shower5,
+  shower6,
+  shower7,
+  stairRailing1,
+  stairRailing2,
+  stairRailing3,
+  stairRailing4,
+  storefront1,
+  storefront2,
+  storefront3,
+  storefront4,
 };
