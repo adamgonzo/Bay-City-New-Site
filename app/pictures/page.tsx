29f94b4a5db255596272cd815a0569545b1e11ad
Adam Gonzalez <adam.gonz@outlook.com>
2023-10-04 21:15:31 -0700

    Changed h1 tag
    Changed heading title to be gallery instead of the original title

--- app/pictures/page.tsx
+++ app/pictures/page.tsx
@@ -4,8 +4,8 @@ export default function Pictures() {
   return (
     <section className="py-12 text-center">
       <div className="mb-8">
-        <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-400 to-cyan-600">
-          Discover Our Stunning Glass Artworks
+        <h1 className="text-5xl font-extrabold border-4 border-black rounded-lg bg-cyan-700 text-white p-4 ">
+          Gallery
         </h1>
       </div>
       <hr className="border-t-2 border-gray-400 mb-8" />
