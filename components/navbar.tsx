220fea3f2ed156b291674c2cf66140ef6b740c37
Adam Gonzalez <adam.gonz@outlook.com>
2023-10-04 21:17:55 -0700

    Changed Look of Navbar
    Changed look of navbar to inlcude the logo and CA licence number

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,9 @@
 "use client";
-
-import clsx from "clsx";
-import { motion } from "framer-motion";
 import Image from "next/image";
+import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import logo from "../images/bayCityGeneral.jpg";
 
 const navItems = {
   "/": {
@@ -14,7 +13,7 @@ const navItems = {
     w: "64px",
   },
   "/pictures": {
-    name: "Pictures",
+    name: "Gallery",
     x: 65,
     y: 36,
     w: "85px",
@@ -27,7 +26,7 @@ const navItems = {
   },
 };
 
-export default function Navbar() {
+const Navbar = () => {
   type NavItemsKeys = keyof typeof navItems;
   let pathname: NavItemsKeys = "/";
   const pathnameFromUsePathname = usePathname();
@@ -40,18 +39,38 @@ export default function Navbar() {
 
   return (
     <aside className="font-serif">
-      <nav className="fixed top-0 left-0 right-0 flex justify-center space-x-8 p-4 text-lg fixed z-50 bg-gray-800 text-white">
-        {/* Nav links */}
-        {Object.entries(navItems).map(([path, { name }]) => (
-          <Link
-            key={path}
-            href={path}
-            className={path === pathname ? "font-bold" : "mx-4 hover:underline"}
-          >
-            {name}
-          </Link>
-        ))}
+      <nav className="fixed top-0 left-0 right-0 flex justify-between items-center p-3 z-50 bg-gray-500 text-white">
+        <div className="flex items-center space-x-2">
+          <Image
+            className="h-20 w-20 mix-blend-multiply"
+            src={logo}
+            alt="logo"
+          />
+          <div className="flex-row-reverse">
+            <span className="text-xl font-bold flex">
+              Bay City General Glass
+            </span>
+            <span className="text-sm font-medium flex text-center font-sans">
+              CA LIC# C17813733
+            </span>
+          </div>
+        </div>
+        <div className="flex space-x-8 text-xl">
+          {Object.entries(navItems).map(([path, { name }]) => (
+            <Link
+              key={path}
+              href={path}
+              className={
+                path === pathname ? "font-bold" : "mx-4 hover:underline"
+              }
+            >
+              {name}
+            </Link>
+          ))}
+        </div>
       </nav>
     </aside>
   );
-}
+};
+
+export default Navbar;
