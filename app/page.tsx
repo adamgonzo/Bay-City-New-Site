8a193c9377da15ff5358cdd69e7fde2ba3629e59
Adam Gonzalez <adam.gonz@outlook.com>
2023-10-12 22:01:27 -0700

    Fixed Home Page Layout
    Fixed home page layout to display company name,
    services
    photo montage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { about } from "@/lib/info";
 import type { Metadata } from "next";
 import glass from "../images/bayCityGeneral.jpg";
+import companyname from "../images/companyName.png";
 import Image from "next/image";
+import ImageMontage from "@/components/image_montage";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://www.baycitygeneralglass.net"),
   description:
     "Glass Installer, Windows, Showers, Glass Railings, Glass Stair Cases, Insulated Glass Units",
   openGraph: {
@@ -19,23 +22,57 @@ export const metadata: Metadata = {
 
 export default function HomePage() {
   return (
-    <section className="flex flex-col items-center justify-center text-white">
-      <div className="mb-2 md:mb-12 relative">
-        <Image
-          alt="Bay City General Glass"
-          src={glass}
-          width={400}
-          height={300}
-          priority
-          className="rounded-lg mix-blend-multiply"
-        />
+    <section className="flex flex-col items-center justify-center min-h-screen w-full max-w-screen-lg mx-auto">
+      <div className="grid grid-cols-3 gap-20 items-center m-auto">
+        <div className="flex flex-col justify-center items-center bg-gray-200 p-4 rounded-lg border-black border-3">
+          <div>
+            <h2 className="text-xl font-semibold mb-4">Services</h2>
+            <ul className="list-disc ml-6 mb-4">
+              <li>Mirrors</li>
+              <li>Showers</li>
+              <li>Insulated Glass</li>
+              <li>Custom Glass Railings</li>
+              <li>Stair Glass Railings</li>
+              <li>Balcony Glass Railings</li>
+              <li>Entry Doors</li>
+              <li>Interior Doors</li>
+              <li>Store Fronts</li>
+              <li>Store Front Glass Replacements</li>
+              <li>Architectural Interior Glass Walls</li>
+            </ul>
+            <a
+              href="/contact"
+              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold no-underline rounded-full py-3 px-6 text-lg transition duration-300 ease-in-out flex items-center justify-center mt-4"
+            >
+              Contact Us
+            </a>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <Image
+            alt="Bay City General Glass"
+            src={glass}
+            width={300}
+            height={300}
+            priority
+            className="rounded-lg mix-blend-multiply"
+            style={{ width: "auto", height: "auto" }}
+          />
+        </div>
+        <div className="flex justify-center items-center bg-gray-200 p-1 rounded-lg border-black border-3">
+          <ImageMontage />
+        </div>
       </div>
-      <div className="text-center mb-4">
-        <h1 className="text-3xl md:text-4xl font-extrabold text-black mb-2 relative flex justify-center">
-          Bay City General Glass
+
+      <div className="mt-6 items-center mx-auto">
+        <h1 className="text-3xl md:text-4xl font-extrabold text-white mb-4 relative flex justify-center">
+          <span className="text-transparent text-shadow-gray-500 font-bold">
+            <Image alt="Bay City General Glass" src={companyname} priority />
+          </span>
         </h1>
-        <hr className="border-black p-3" />
-        <p className="text-md md:text-lg text-black mb-2 pt-5">{about()}</p>
+        <hr className="border-black border-3" />
+        <p className="text-md md:text-lg text-black mb-4 pt-5">{about()}</p>
       </div>
     </section>
   );
