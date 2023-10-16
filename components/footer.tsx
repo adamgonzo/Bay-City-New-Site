0cc309e5b7c9471de337a34243e7510d1f36e07e
Adam Gonzalez <adam.gonz@outlook.com>
2023-10-16 16:31:55 -0700

    fixed

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,17 +6,21 @@ import logo from "../images/bayCityGeneral.jpg";
 
 export default function Footer() {
   return (
-    <footer className="bg-white text-black py-4">
+    <footer className="bg-white text-black py-2">
       <Container fluid className="text-center">
-        <Row className="mb-3 flex">
-          <Col xs={12} sm={6} className="flex mb-3 mb-sm-0 text-sm text-center">
+        <Row className="mb-3 flex justify-between">
+          <Col
+            xs={12}
+            sm={6}
+            className="flex mb-3 mb-sm-0 text-sm text-center md:justify-start lg:justify-start justify-center"
+          >
             <div className="flex items-center justify-center gap-3">
               <Image
                 src={logo}
                 alt="Bay City General Glass"
                 height={40}
                 width={40}
-                className="mix-blend-multiply"
+                className="mix-blend-multiply hidden md:block lg:block"
               />
               <div>
                 <h1 className="text-sm mb-1">Bay City General Glass</h1>
@@ -27,22 +31,28 @@ export default function Footer() {
             </div>
           </Col>
 
-          <Col xs={12} sm={6} className="flex items-center justify-end">
-            <a href="/" className="text-black text-decoration-none mx-3">
-              Home
-            </a>
-            <a
-              href="/pictures"
-              className="text-black text-decoration-none mx-3"
-            >
-              Gallery
-            </a>
-            <a href="/contact" className="text-black text-decoration-none mx-3">
-              Contact
-            </a>
-            <Button href="tel:+15102587839" className="rounded p-1">
-              Call Now
-            </Button>
+          <Col
+            xs={12}
+            sm={6}
+            className="flex items-center md:justify-end lg:justify-end justify-center "
+          >
+            <div className="flex">
+              <a href="/" className="text-black text-decoration-none mx-3">
+                Home
+              </a>
+              <a
+                href="/pictures"
+                className="text-black text-decoration-none mx-3"
+              >
+                Gallery
+              </a>
+              <a
+                href="/contact"
+                className="text-black text-decoration-none mx-3"
+              >
+                Contact
+              </a>
+            </div>
           </Col>
         </Row>
         <Row>
