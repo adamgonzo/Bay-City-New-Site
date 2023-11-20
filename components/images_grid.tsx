5d261da087146975c47b4b494cb939d0e65c7d79
Adam <86032875+adamgonzo@users.noreply.github.com>
2023-11-19 21:17:20 -0800

    updated pictures for seo

--- components/images_grid.tsx
+++ components/images_grid.tsx
@@ -1,11 +1,9 @@
-"use client";
-import React, { useState } from "react";
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react'
+import Image from 'next/image'
 import {
   stairRailing1,
   stairRailing2,
-  stairRailing3,
-  stairRailing4,
   shower1,
   shower2,
   shower3,
@@ -16,46 +14,103 @@ import {
   storefront1,
   storefront2,
   storefront3,
-  storefront4,
-} from "../images";
+  storefront4
+} from '../images'
 
-const categories = ["All", "Shower", "Storefront", "Stair Railing"];
+const categories = ['All', 'Shower', 'Storefront', 'Stair Railing']
 
 export default function Gallery() {
-  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+  const [selectedCategory, setSelectedCategory] = useState(categories[0])
 
   const allPictures = [
-    shower1,
-    shower2,
-    shower3,
-    shower4,
-    shower5,
-    shower6,
-    shower7,
-    storefront1,
-    storefront2,
-    storefront3,
-    storefront4,
-    stairRailing1,
-    stairRailing2,
-    stairRailing3,
-    stairRailing4,
-  ];
+    {
+      img: shower1,
+      title: 'Corner Shower',
+      description:
+        'Corner glass shower with header in matte black in the upstairs bathroom'
+    },
+    {
+      img: shower2,
+      title: '90 Degree Frameless Shower Unit',
+      description:
+        'Frameless shower unit with chrome hardware two notched panels and one stationary panel with door in middle.'
+    },
+    {
+      img: shower3,
+      title: 'Another Angle Of The 90 Degree Shower Unit',
+      description: 'Frameless shower unit with chrome hardware from the front'
+    },
+    {
+      img: shower4,
+      title: 'Corner Shower',
+      description:
+        'Corner shower with matte black header at the downstairs bathroom'
+    },
+    {
+      img: shower5,
+      title: 'Serenity Shower Slider',
+      description: 'Installed serenity shower on a bathtub with towel bars'
+    },
+    {
+      img: shower6,
+      title: 'Shower Slider',
+      description:
+        'Installed shower slider with stationary panel in tile enclosed shower unit with handle on left'
+    },
+    {
+      img: shower7,
+      title: '90 Degree Serenity Shower',
+      description:
+        '90 Degree serenity shower with chrome hardware slider closing into 90 degree panel '
+    },
+    {
+      img: storefront1,
+      title: 'Green store front with grid pattern',
+      description: 'Store front installed with aluminum material'
+    },
+    {
+      img: storefront2,
+      title: 'Commercial Store Front',
+      description:
+        'Store front installed with aluminum hardware, and also installed double doors for entrance with panic hardware.'
+    },
+    {
+      img: storefront3,
+      title: 'Store Front',
+      description:
+        'Store front with grid pattern for top windows and store front windows'
+    },
+    {
+      img: storefront4,
+      title: 'Store front door',
+      description: 'Store front door with panic hardware and top window'
+    },
+    {
+      img: stairRailing1,
+      title: 'Glass stair railing',
+      description: 'Curved glass stair railing with posts and a top rail'
+    },
+    {
+      img: stairRailing2,
+      title: 'Glass Stairs',
+      description: 'Glass stairs with standoffs'
+    }
+  ]
 
   const filteredPictures = () => {
     switch (selectedCategory.toLowerCase()) {
-      case "all":
-        return allPictures;
-      case "shower":
-        return allPictures.slice(0, 7);
-      case "storefront":
-        return allPictures.slice(7, 11);
-      case "stair railing":
-        return allPictures.slice(11);
+      case 'all':
+        return allPictures
+      case 'shower':
+        return allPictures.slice(0, 7)
+      case 'storefront':
+        return allPictures.slice(7, 11)
+      case 'stair railing':
+        return allPictures.slice(11)
       default:
-        return [];
+        return []
     }
-  };
+  }
 
   return (
     <div className="p-4">
@@ -65,7 +120,7 @@ export default function Gallery() {
         </label>
         <select
           id="category"
-          onChange={(e) => setSelectedCategory(e.target.value)}
+          onChange={e => setSelectedCategory(e.target.value)}
           value={selectedCategory}
           className="p-2 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
         >
@@ -77,21 +132,30 @@ export default function Gallery() {
         </select>
       </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 auto-cols-fr gap-3">
-        {filteredPictures().map((img, index) => (
+        {filteredPictures().map((photo, index) => (
           <div
-            className="h-80 relative rounded-lg overflow-hidden hover:shadow-lg transition duration-300"
+            className="h-full  rounded-lg overflow-hidden shadow-lg transition duration-300 dark:shadow-gray-300 shadow-black "
             key={index}
           >
-            <Image
-              className="h-full w-full object-fill"
-              src={img}
-              alt={`Gallery Image ${index}`}
-              fill={true}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
+            <div className="p-3 flex flex-col h-full justify-between">
+              <Image
+                className="h-fit w-full object-contain mb-4 rounded-lg"
+                src={photo.img}
+                alt={`Gallery Image ${index}`}
+              />
+              <div className="mb-auto mt-auto">
+                <h2 className="text-xl font-semibold mt-auto">{photo.title}</h2>
+              </div>
+              <div className="mb-auto mt-auto">
+                <hr className="border dark:border-gray-200 border-black" />
+              </div>
+              <div className="mb-auto mt-auto">
+                <p className="text-sm">{photo.description}</p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
     </div>
-  );
+  )
 }
